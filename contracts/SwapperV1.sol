18a1e1d50dda00a29a569bf501e4f1c1fb7734bd
franklin <f.j.a.8421@gmail.com>
2022-03-16 23:36:55 -0400

    finish

---
+++ contracts/SwapperV1.sol
@@ -0,0 +1,148 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol"; 
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol"; 
+import "hardhat/console.sol";
+
+import "./interfaces/IUniswapV2Exchange.sol";
+import "./interfaces/IUniswapV2Router.sol";
+import "./interfaces/IUniswapV2Factory.sol";
+import "./interfaces/IWETH.sol";
+import "./interfaces/IBalancerRegistry.sol";
+import "./interfaces/IBalancerPool.sol";
+
+
+contract SwapperV1 is Initializable, AccessControlUpgradeable, UUPSUpgradeable{
+
+    /// @notice declaration of variables
+
+    using Counters for Counters.Counter;
+    uint256 public commission;  
+    Counters.Counter private _itemIds; 
+    address public target_account;
+    address public _owner;
+
+
+    using SafeMath for uint256;
+    using UniswapV2ExchangeLib for IUniswapV2Exchange;
+
+
+    IUniswapV2Factory internal constant factory =
+        IUniswapV2Factory(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f);
+
+    IWETH internal constant WETH =
+        IWETH(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+
+    // Receives 0.1% of the total ETH used for swaps
+    address public feeRecipient;
+
+    // fee charged, initializes in 0.1%
+    uint256 public fee;
+
+
+
+    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
+
+
+
+    /// @notice events
+
+    event exchange(
+        string,
+        address[]  tokens, 
+        uint256[]  distribution
+    );
+   
+
+    /// @notice contract initialization
+
+    function initialize(address _feeRecipient, uint256 _fee) public initializer {
+
+        require(_feeRecipient != address(0));
+        require(_fee > 0);
+        feeRecipient = _feeRecipient;
+        fee = _fee;
+
+
+        __AccessControl_init();
+        __UUPSUpgradeable_init();
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(UPGRADER_ROLE, msg.sender);
+        
+        
+    }
+
+    function _authorizeUpgrade(address newAuthorize) internal onlyRole(UPGRADER_ROLE) override{}
+
+
+     /**
+    @notice make a swap using uniswap
+   */
+    function _swapUniswap(
+        IERC20 fromToken,
+        IERC20 destToken,
+        uint256 amount
+    ) internal returns (uint256 returnAmount) {
+        require(fromToken != destToken, "SAME_TOKEN");
+        require(amount > 0, "ZERO-AMOUNT");
+
+
+
+        IUniswapV2Exchange exchange = factory.getPair(fromToken, destToken);
+        returnAmount = exchange.getReturn(fromToken, destToken, amount);
+
+        fromToken.transfer(address(exchange), amount);
+
+        if (
+            uint256(uint160(address(fromToken))) <
+            uint256(uint160(address(destToken)))
+        ) {
+            exchange.swap(0, returnAmount, msg.sender, "");
+        } else {
+            exchange.swap(returnAmount, 0, msg.sender, "");
+        }
+    }
+
+    /**
+    @notice swap ETH for multiple tokens according to distribution %
+    @dev tokens length should be equal to distribution length
+    @dev msg.value will be completely converted to tokens
+    @param tokens array of tokens to swap to
+    @param distribution array of % amount to convert eth from (3054 = 30.54%)
+   */
+    function swap(address[] memory tokens, uint256[] memory distribution)
+        external
+        payable
+    {
+        require(msg.value > 0);
+        require(tokens.length == distribution.length);
+
+        // Calculate ETH left after subtracting fee
+        uint256 afterFee = msg.value.sub(msg.value.mul(fee).div(100000));
+
+        // Wrap all ether that is going to be used in the swap
+        WETH.deposit{value: afterFee}();
+        emit exchange("exchange",tokens,distribution);
+
+        for (uint256 i = 0; i < tokens.length; i++) {
+            _swapUniswap(
+                WETH,
+                IERC20(tokens[i]),
+                afterFee.mul(distribution[i]).div(10000)
+            );
+        }
+
+        // Send remaining ETH to fee recipient
+        payable(feeRecipient).transfer(address(this).balance);
+    }
+
+
+
+
+
+}
+
---
+++ test/test.js
@@ -0,0 +1,145 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const axios = require("axios");
+
+const SwapperV1 = artifacts.require("SwapperV1");
+const SwapperV2 = artifacts.require("SwapperV2");
+const IBalancerRegistry = artifacts.require("IBalancerRegistry");
+const IUniswapV2Factory = artifacts.require("IUniswapV2Factory");
+const IERC20 = artifacts.require("IERC20");
+
+const WETH_ADDRESS = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+const DAI_ADDRESS = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+const LINK_ADDRESS = "0x514910771AF9Ca656af840dff83E8264EcF986CA";
+const USDT_ADDRESS = "0xdac17f958d2ee523a2206206994597c13d831ec7";
+
+const BALANCER_REGISTRY = "0x65e67cbc342712DF67494ACEfc06fe951EE93982";
+const UNI_FACTORY = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
+
+// HELPERS
+const toWei = (value) => web3.utils.toWei(String(value));
+const fromWei = (value) => Number(web3.utils.fromWei(String(value)));
+
+const FEE = 100; // 0.1%
+
+
+contract("Swapper", ([user, feeRecipient]) => {
+  let swapper, uniRouter, dai, link;
+
+  before(async function () {
+    dai = await IERC20.at(DAI_ADDRESS);
+    link = await IERC20.at(LINK_ADDRESS);
+    usdt = await IERC20.at(USDT_ADDRESS);
+    balancer = await IBalancerRegistry.at(BALANCER_REGISTRY);
+    factory = await IUniswapV2Factory.at(UNI_FACTORY);
+  });
+
+  it("Should deploy proxy with V1", async function () {
+    const SwapperV1Factory = await ethers.getContractFactory("SwapperV1");
+
+    const proxy = await upgrades.deployProxy(SwapperV1Factory, [
+      feeRecipient,
+      FEE,
+    ]);
+    swapper = await SwapperV1.at(proxy.address);
+  });
+
+
+  it("Should use swapper tool", async function () {
+    const distribution = [3000, 7000]; // 30% and 70%
+    const tokens = [DAI_ADDRESS, LINK_ADDRESS];
+    const intialFeeRecipientBalance = await web3.eth.getBalance(feeRecipient);
+
+    const tx = await swapper.swap(tokens, distribution, { value: toWei(2) });
+
+    const balanceDAI = await dai.balanceOf(user);
+    const balanceLINK = await link.balanceOf(user);
+  
+    const contractBalance = await web3.eth.getBalance(swapper.address);
+    const finalFeeRecipientBalance = await web3.eth.getBalance(feeRecipient);
+
+    assert.notEqual(balanceDAI, 0);
+    assert.notEqual(balanceLINK, 0);
+    assert.equal(contractBalance, 0);
+
+    assert(
+      fromWei(finalFeeRecipientBalance) > fromWei(intialFeeRecipientBalance)
+    );
+  });
+
+
+
+  it("Should upgrade to V2", async function () {
+    const SwapperV2Factory = await ethers.getContractFactory("SwapperV2");
+
+    const proxy = await upgrades.upgradeProxy(
+      swapper.address,
+      SwapperV2Factory
+    );
+    swapper = await SwapperV2.at(proxy.address);
+  });
+
+  it("Should swap 2 tokens using best dex", async function () {
+    const TOKENS = [DAI_ADDRESS, LINK_ADDRESS];
+    const AMOUNT = [0.3, 0.7];
+    const DISTRIBUTIONS = [3000, 7000];
+
+    const swaps = [];
+
+    for (let i = 0; i < TOKENS.length; i++) {
+      const token = TOKENS[i];
+      const amount = AMOUNT[i];
+      const distribution = DISTRIBUTIONS[i];
+
+      const { data } = await axios.get(
+        `https://api.1inch.exchange/v3.0/1/quote?fromTokenAddress=${WETH_ADDRESS}&toTokenAddress=${token}&amount=${toWei(
+          amount
+        )}&protocols=UNISWAP_V2,BALANCER`
+      );
+
+      let swapData;
+
+      if (data.protocols[0][0][0].name === "UNISWAP_V2") {
+        const pool = await factory.getPair(WETH_ADDRESS, token);
+        swapData = { token, pool, distribution, dex: 0 };
+      } else {
+        const pools = await balancer.getBestPoolsWithLimit(
+          WETH_ADDRESS,
+          token,
+          1
+        );
+        swapData = { token, pool: pools[0], distribution, dex: 1 };
+      }
+
+      swaps.push(swapData);
+    }
+
+    const intialFeeRecipientBalance = await web3.eth.getBalance(feeRecipient);
+
+    const tx = await swapper.swapMultiple(swaps, {
+      value: toWei(1),
+    });
+
+    const balanceDAI = await dai.balanceOf(user);
+    const balanceLINK = await link.balanceOf(user);
+    const contractBalance = await web3.eth.getBalance(swapper.address);
+    const finalFeeRecipientBalance = await web3.eth.getBalance(feeRecipient);
+
+    assert.notEqual(balanceDAI, 0);
+    assert.notEqual(balanceLINK, 0);
+    assert.equal(contractBalance, 0);
+    assert(
+      fromWei(finalFeeRecipientBalance) > fromWei(intialFeeRecipientBalance)
+    );
+  });
+
+
+});
+
+
+
+
+
+
+
+
